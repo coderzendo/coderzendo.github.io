69c6ab251c2d58fe25db17d47a306025ba6d17cd
Charles Calvert at School <charles.calvert@bellevuecollege.edu>
2018-11-07 12:22:04 -0800

    statujs

--- javascript-test/function-basics.js
+++ javascript-test/function-basics.js
@@ -1,21 +1,40 @@
+window.onload = () => {
+    
+    const functionCalls = document.getElementById('function-calls');
+    const callUserInput = document.getElementById('call-user-input');
+
+    functionCalls.onclick = () => {
+        simpleFunction();  
+        functionParameters("Many functions take parameters.");
+        functionReturn();
+        const returnValue = functionReturn();
+        console.log(returnValue);
+    }    
+
+    callUserInput.onclick = () => {
+        const userInputParagraph = document.getElementById('show-user-input');
+        const userInput = document.getElementById('user-input');
+        userInputParagraph.textContent = userInput.value;
+    }
+}
+
 function simpleFunction() {
-    console.log("functions should consist of statements designed to perform a single task.");
-}
-
-function add(a, b) {
-    console.log(a + b);
-}
-
-function add() {
-    console.log(2 + 3);
+    const displayText = "functions should consist of statements designed to perform a single task.";
+    const simpleFunctionParagraph = document.getElementById("simple-function");
+    console.log(displayText);
+    simpleFunctionParagraph.textContent = displayText;
 }
 
 function  functionParameters(value) {
-    console.log(value)
+    const functionParametersParagraph = document.getElementById("function-parameters");
+    console.log(value);
+    functionParametersParagraph.textContent = value;
 }
 
-simpleFunction();
-functionParameters("Many functions take parameters.");
-add(6, 3);
-add();
+function functionReturn() {
+    return "Many functions return values.";
+}
+
+// simpleFunction();
+//functionParameters("Many functions take parameters.");
 // adder(4, 9);
