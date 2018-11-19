d4c4fca0e23af89bb4b4df636721c2714f0e4c74
Charlie on Linux Ubuntu Main <charlie@elvenware.com>
2018-11-19 08:43:10 -0800

    arrays

---
+++ javascript-test/javascript-arrays.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+
+};
+
+const arrays = {
+    numbers: [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+};
+
+for (let number of arrays.numbers) {
+    console.log(number);
+}
