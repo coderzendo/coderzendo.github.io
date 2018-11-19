dc10893f24eb8e2a1df31516c5e3724f5e8be45c
Charlie on Linux Ubuntu Main <charlie@elvenware.com>
2018-11-19 09:16:22 -0800

    coderzend

--- javascript-test/javascript-arrays.js
+++ javascript-test/javascript-arrays.js
@@ -1,11 +1,23 @@
 window.onload = () => {
 
-};
+    const arrays = {
+        numbers: [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+        languageNames: [],
+        languagePopularity: []
+    };
 
-const arrays = {
-    numbers: [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-};
+    const numbersAction = document.getElementById('numbers-action');
+    const languageNamesAction = document.getElementById('tech-language-names-action');
+    const techLanguagePopularityAction = document.getElementById('tech-language-popularity-action');
 
-for (let number of arrays.numbers) {
-    console.log(number);
-}
+    numbersAction.onclick = () => {
+        const numbersDisplay = document.getElementById('numbers-display');
+        for (let number of arrays.numbers) {
+            const li = document.createElement("li");
+            li.appendChild(document.createTextNode(number));
+            numbersDisplay.appendChild(li);
+        }
+    };
+    languageNamesAction.onclick = () => {};
+    techLanguagePopularityAction.onclick = () => {};
+};
