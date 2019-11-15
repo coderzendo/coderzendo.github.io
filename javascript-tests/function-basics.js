08e7a6f70440d02a689aa5cdcdd0780698d15c91
Charlie on Linux Ubuntu Main <charlie@elvenware.com>
2019-11-15 10:54:55 -0800

    reformat of code: 11-15-2019

--- javascript-tests/function-basics.js
+++ javascript-tests/function-basics.js
@@ -2,8 +2,10 @@ window.onload = () => {
     // All code goes in here.
     const simpleFunctionDisplay = document.getElementById('simpleFunctionDisplay');
     const functionCallsAction = document.getElementById('functionCallsAction');
-    
-    functionCallsAction.onclick = simpleFunction;
+
+    functionCallsAction.onclick = function () {
+        simpleFunction();
+    }
 
     function simpleFunction() {
         console.log("functions should consist of statements designed to perform a single task.");
@@ -11,28 +13,28 @@ window.onload = () => {
     }
 
 
-function functionParameters(value) {
-    console.log(value);
-}
+    function functionParameters(value) {
+        console.log(value);
+    }
 
-function functionReturn(addResult) {
-    return "The add function returned " + addResult;
-}
+    function functionReturn(addResult) {
+        return "The add function returned " + addResult;
+    }
 
-function add(a, b) {
-    const addResult = a + b;
-    console.log('Inside add', addResult);
-    return addResult;
-}
+    function add(a, b) {
+        const addResult = a + b;
+        console.log('Inside add', addResult);
+        return addResult;
+    }
 
-//simpleFunction();
+    //simpleFunction();
 
-functionParameters("Many functions take parameters.");
+    functionParameters("Many functions take parameters.");
 
-const addResult = add(2, 3);
-console.log('Add Result', addResult);
-const returnValue = functionReturn(addResult);
+    const addResult = add(2, 3);
+    console.log('Add Result', addResult);
+    const returnValue = functionReturn(addResult);
 
-console.log(returnValue);
+    console.log(returnValue);
 
 }
