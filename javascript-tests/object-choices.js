8bf76f1f639ea92677db59da3dade7a20f8589f5
Charlie Calvert on School Machine <charlie@elvenware.com>
2019-11-20 12:08:44 -0800

    foo

--- javascript-tests/object-choices.js
+++ javascript-tests/object-choices.js
@@ -1,30 +1,42 @@
-const simpleObject = {
-    sayName: function() {
-        console.log("Simple Object");
+window.onload = () => {
+
+    const simpleObjectDisplay = document.getElementById('simpleObjectDisplay');
+    const simpleObjectAction = document.getElementById('simpleObjectAction');
+    //simpleObjectDisplay.textContent = 'foobar';
+
+    const simpleObject = {
+        firstName: 'James',
+        lastName: 'Watson',
+        sayName: function () {
+            console.log(this.firstName + " " + this.lastName);
+        }
+    };
+
+    simpleObjectAction.onclick = simpleObject.sayName;
+
+    simpleObject.dynamicMethod = () => {
+        console.log("Dynamic Method");
     }
-};
 
-simpleObject.dynamicMethod = () => {
-    console.log("Dynamic Method");
-}
-
-function FunctionObject() {
-    FunctionObject.prototype.sayName = function() {
-        console.log('Function Object');
+    function FunctionObject() {
+        FunctionObject.prototype.sayName = function () {
+            console.log('Function Object');
+        }
     }
-}
 
-class CustomClass {
-    sayName() {
-        console.log("CustomClass");
+    class CustomClass {
+        sayName() {
+            console.log("CustomClass");
+        }
     }
+
+    simpleObject.sayName();
+    simpleObject.dynamicMethod();
+
+    const functionObject = new FunctionObject();
+    functionObject.sayName();
+
+    const customClass = new CustomClass();
+    customClass.sayName();
+
 }
-
-simpleObject.sayName();
-simpleObject.dynamicMethod();
-
-const functionObject = new FunctionObject();
-functionObject.sayName();
-
-const customClass = new CustomClass();
-customClass.sayName();
