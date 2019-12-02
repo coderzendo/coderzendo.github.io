5c912d083bcf334f11049261cf2b6581376942e2
Charlie Calvert on School Machine <charlie@elvenware.com>
2019-12-02 12:24:47 -0800

    done

---
+++ javascript-tests/test.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+    const numbers = [1, 2, 3];
+
+    // Many lines of code
+
+    function foo() {
+        console.log('foo');
+    }
+
+    const myObject = {
+        width: 2,
+        bar: function() {
+            console.log('bar');
+        }  
+    }
+}
