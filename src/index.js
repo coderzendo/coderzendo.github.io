204f483ee8d952d9d0083080879a617ba1940e0f
charlie on ubuntu 22.04 <charlie@elvenware.com>
2023-02-08 00:51:48 -0800

    much cleaner but not done

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 // src/index.js
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore/lite';
 import { firebaseConfig } from './configure';
-import { runSacramento } from './set_document';
 import { signInElf } from './sign-in-elf';
-import { phaserData } from './set_document';
-import { phaserDataSetup } from './phaser-data';
 
 
 const firebaseApp = initializeApp(firebaseConfig);
-const db = getFirestore(firebaseApp);
 signInElf(firebaseApp);
-phaserData(firebaseApp);
-phaserDataSetup(firebaseApp);
-console.log('src/index.js fom bundle called');
-runSacramento(firebaseApp);
+
+console.log('src/index.js from bundle called');
+
--- test.sh
+++
@@ -1,4 +0,0 @@
-// cd -- "$(mktemp -d)" 
-printf '%s\n' 'nuts' 'bolts' > first_file.txt
-printf '%s\n' 'foo' 'bar' 'baz' > second_file.txt
-sed -e '/bar/r./first_file.txt' second_file.txt
